3a596edcb28fe194228a0ff0f9ce8bfb0324cbde
Talamortis <talamortis@gmail.com>
2023-08-27 18:23:44 +0100

    revert of "adding module config, TODO sql update ExecuteFile (#2)"

--- modules/mod_sample/src/mod_sample.cpp
+++ modules/mod_sample/src/mod_sample.cpp
@@ -1,4 +1,5 @@
 #include "ScriptDevMgr.h"
+#include "World.h"
 
 class modsample : public PlayerScript
 {
@@ -7,11 +8,17 @@ public:
 
     void OnLogin(Player* player)
     {
+        if (!sWorld.GetModuleBoolConfig("module_sample", false))
+            return;
+
         ChatHandler(player).PSendSysMessage("Hook works fine!");
     }
 
     void OnGiveXP(Player* player, uint32& xp, Unit* victim)
     {
+        if (sWorld.GetModuleBoolConfig("module_sample", false))
+            return;
+
         ChatHandler(player).PSendSysMessage("You just recieved %u xp", xp);
     }
 };
